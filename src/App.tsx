419c131eb0f7adb0561e6fb878d183aba3de7cac
Alpo Banerjee <alpobanerjee374@gmail.com>
2026-07-26 18:24:50 +0530

    refactor: remove Vite boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import './App.css'
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
-  )
+  return <Home />
 }
 
 export default App
